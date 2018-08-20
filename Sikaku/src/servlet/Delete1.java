1d2fa63ea0977cc9929538636dfe484687ca5f9d
seiya hatae <seiyahatae1107@gmail.com>
2018-08-20 19:41:34 +0900

    削除機能追加

---
+++ Sikaku/src/servlet/Delete1.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.SikakuDao;
+
+/**
+ * Servlet implementation class Insert
+ */
+@WebServlet("/Delete1")
+public class Delete1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Delete1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//formに入力された検索キーを取得
+				request.setCharacterEncoding("UTF-8");
+				String id = request.getParameter("id");
+				int id1 = Integer.parseInt(id);
+
+				SikakuDao.Delete1Dao(id1);
+				String view = "/WEB-INF/view/result2.jsp";
+				RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+				dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
